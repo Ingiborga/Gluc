0b8a349007b522716f8d84c7888fc571c05806ec
Ingiborga <spora2001@mail.ru>
2026-07-11 18:12:49 +0300

    activity настроила и начала sharedpreferences

--- app/src/main/java/com/test/AuthActivity.java
+++ app/src/main/java/com/test/AuthActivity.java
@@ -1,7 +1,11 @@
 package com.test;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,20 +14,57 @@ import com.test.broadcast.AidexBroadcastReceiver;
 import com.test.broadcast.BroadcastService;
 
 public class AuthActivity extends AppCompatActivity {
-    private TextView user_email;
-    private TextView user_pass;
+
+    private SharedPreferences prefs;
+    private  SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_login);
+        setContentView(R.layout.activity_login);
+        initPrefs();
 
-        //добавить про радиогрупп!!!
-
-        user_email = findViewById(R.id.editTextTextEmailAddress);
-        user_pass = findViewById(R.id.editTextTextPassword);
-
-        //проверка, если данные есть, то переход на следующую страницу
+        if (prefs.contains("email")) {
+            // Настройки есть
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+        //radiogroup
+        findViewById(R.id.ottai).setOnClickListener((view)->onRadioButtonClicked(view));
+        // устанавливаем обработчики для кнопок
+        findViewById(R.id.aidex).setOnClickListener((view)->onRadioButtonClicked(view));
     }
+    private void initPrefs() {
+        prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        editor = prefs.edit();
+    }
+    public void onRadioButtonClicked(View view) {
+        RadioButton radio = (RadioButton) view;
+        // если переключатель отмечен
+        boolean checked = radio.isChecked();
+        // получаем текст нажатой радиокнопки
+        String text = radio.getText().toString();
+        // Получаем нажатый переключатель
+        switch(text) {
+            case "Ottai":
+                editor.putString("glucometer", "ottai");
+                break;
+            case "Aidex":
+                editor.putString("glucometer", "aidex");
+                break;
+        }
+    }
+    public void PushButton(View view){
+        EditText user_email = findViewById(R.id.editTextTextEmailAddress);
+        EditText user_pass = findViewById(R.id.editTextTextPassword);
+        //сохранение в память
+        editor.putString("email", user_email.getText().toString());
+        editor.putString("password", user_pass.getText().toString());
+        editor.apply();
+        startActivity(new Intent(this, MainActivity.class));
+        overridePendingTransition(0, 0);
+        finish();
+    }
+
     @Override
     protected void onResume() {//при запуске страницы запускаются сервисы по чтению глюкозы
         super.onResume();
--- app/src/main/java/com/test/ProfileActivity.java
+++ app/src/main/java/com/test/ProfileActivity.java
@@ -1,7 +1,11 @@
 package com.test;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,18 +17,33 @@ import com.test.broadcast.BroadcastService;
 public class ProfileActivity extends AppCompatActivity {
     private TextView user_email;//логин
     private TextView user_pass;
+    private SharedPreferences prefs;
+    private  SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_account);
+        setContentView(R.layout.activity_account);
+        initPrefs();
+        TextView user_login=findViewById(R.id.user_login);
+        user_login.setText(prefs.getString("email", ""));
 
+        TextView upper_limit_glucose=findViewById(R.id.upper_limit_glucose);
+        upper_limit_glucose.setText(prefs.getString("upper_limit_glucose", ""));
 
+        TextView lower_limit_glucose=findViewById(R.id.lower_limit_glucose);
+        lower_limit_glucose.setText(prefs.getString("lower_limit_glucose", ""));
 
-        //добавить про радиогрупп!!!
-        //и нижнюю границу
+        findViewById(R.id.ottai).setOnClickListener((view)->onRadioButtonClicked(view));
+        // устанавливаем обработчики для кнопок
+        findViewById(R.id.aidex).setOnClickListener((view)->onRadioButtonClicked(view));
+        RadioGroup radioGroup = findViewById(R.id.group);
+        String sensorType = prefs.getString("glucometer", "aidex");
+        if (sensorType.equals("ottai")) {
+            radioGroup.check(R.id.ottai);
+        } else {
+            radioGroup.check(R.id.aidex);
+        }
 
-        //lower_bound = findViewById(R.id.editTextTextEmailAddress);
-        //user_pass = findViewById(R.id.editTextTextPassword);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.action_profile);
 
@@ -50,6 +69,26 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
         //проверка, если данные есть, то переход на следующую страницу
+    }
+    private void initPrefs() {
+        prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        editor = prefs.edit();
+    }
+    public void onRadioButtonClicked(View view) {
+        RadioButton radio = (RadioButton) view;
+        // если переключатель отмечен
+        boolean checked = radio.isChecked();
+        // получаем текст нажатой радиокнопки
+        String text = radio.getText().toString();
+        // Получаем нажатый переключатель
+        switch(text) {
+            case "Ottai":
+                editor.putString("glucometer", "ottai");
+                break;
+            case "Aidex":
+                editor.putString("glucometer", "aidex");
+                break;
+        }
     }
     @Override
     protected void onResume() {//при запуске страницы запускаются сервисы по чтению глюкозы
